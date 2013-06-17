97710b9072a1da7979950ef9b2a2c809c06e176c
Nick McCready <nmccready@gmail.com>
2013-06-17 14:19:56 -0400

    rspec for talking to a real rtsp server working. It is targeting the WOZA server at http://www.wowza.com

--- spec/conf/config.rb
+++ spec/conf/config.rb
@@ -1,7 +1,10 @@
 require 'configatron'
 
 
-configatron.rtsp_server.url='http://localhost'
-
+# ip address is only needed if it is a real rtsp connection
+# the client will default to port 554
+#
+#woza rtsp ec2 server
+configatron.rtsp_server_wowza.url='184.72.239.149'
 configatron.fake_rtsp_server.url='http://localhost'
 
